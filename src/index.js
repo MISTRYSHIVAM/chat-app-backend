b1f5f7d59ca08ac3d28234bcc95f55a12d7f768e
shivam <shivammistry2003@gmail.com>
2025-01-29 16:35:14 +0530

    welcome route

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { connectToDb } from './db/index.js';
 dotenv.config()
 const PORT = process.env.APP_PORT;
 
-export default (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send('Hello, world!');
-};
+});
 connectToDb().then(() => {
     app.listen(PORT, () => {
         console.log(`app is runnig in port ${PORT}`);
