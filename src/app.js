3c315aacf4e1fcec4b6c997421563c383e9664f8
shivam <shivammistry2003@gmail.com>
2025-01-29 15:34:00 +0530

    add vercel.json configuration file

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.use(express.json());
 
 app.use(cors({ origin: '', credentials: true }));
 
+app.get('/', (req, res) => {
+    res.json({ statusCode: 200, message: "Welcome To Deployment Server" })
+})
+
 export { app };
